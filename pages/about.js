8b503df36451f6c462027b6953ad5b4ea13b73d0
Ryan <rrichetto@comcast.net>
2021-02-19 18:09:21 -0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ad
+        dicta id iusto, veniam molestiae sequi illum? Voluptas ratione dolor
+        labore aspernatur, nisi quis fugit quo! Doloremque, dolorem?
+        Praesentium, totam.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ad
+        dicta id iusto, veniam molestiae sequi illum? Voluptas ratione dolor
+        labore aspernatur, nisi quis fugit quo! Doloremque, dolorem?
+        Praesentium, totam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
